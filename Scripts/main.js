1e828df82f4b941074a6365c90bc70a3db5ed1b5
etinyeneuko <etinyeneuko@gmail.com>
2019-09-04 14:09:40 +0100

    I've added main.js file

---
+++ Scripts/main.js
@@ -0,0 +1,5 @@
+
+    document.addEventListener('DOMContentLoaded', function () {
+                  var elems = document.querySelectorAll('select');
+    var instances = FormSelect.init(elems, options);
+  });
