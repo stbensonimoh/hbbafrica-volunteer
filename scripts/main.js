5969f5fd82642fb897bbd4fd79d30c214fae559b
Benson Imoh,ST <benson@stbensonimoh.com>
2019-09-07 18:14:09 +0100

    Fix display issues
    - Add International Telephone number plugin to the phone input field
      It will help users input their numbers in the correct format
    
    - Fix the display of the form by applying the appropriate grid system,
      making the form responsive
    
    - Make the logo image responsive by using rem instead of absolute values
    
    - Add author(s) atttribution

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,19 @@
 'use strict'
 document.addEventListener('DOMContentLoaded', () => {
+  let date = new Date()
+  const year = date.getFullYear()
+  let yearContent = document.querySelector('#year')
+  yearContent.innerHTML = year
+
+  // Add intl-tel-input
+  window.intlTelInputGlobals.loadUtils('scripts/utils.js')
+  var input = document.querySelector('#phone')
+  window.intlTelInput(input, {
+    initialCountry: 'ng',
+    separateDialCode: true,
+    hiddenInput: 'full_phone',
+    utilsScript: 'scripts/utils.js'
+  })
   const elems = document.querySelectorAll('select')
   const instances = M.FormSelect.init(elems)
 })
