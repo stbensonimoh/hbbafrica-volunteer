a909c7136f32b7244f500317a4443d81e272e18c
Benson Imoh,ST <benson@stbensonimoh.com>
2019-09-07 20:59:10 +0100

    Fix required not working on <select> and prevent default form submit

--- scripts/main.js
+++ scripts/main.js
@@ -18,4 +18,27 @@ document.addEventListener('DOMContentLoaded', () => {
   const instances = M.FormSelect.init(elems)
   const count = document.querySelector('textarea#textarea2')
   M.CharacterCounter.init(count)
+
+  //   Submit the form
+  const form = document.querySelector('form')
+
+  // On Form Submit
+  form.addEventListener('submit', e => {
+    // Check to see if form has validation errors
+    if (form.checkValidity() === false) {
+      e.preventDefault()
+      e.stopPropagation()
+    }
+
+    // If form doesn't have validation errors
+    if (form.checkValidity() === true) {
+      e.preventDefault()
+
+      // change the button color and add the loading class
+      document.querySelector('button').classList.remove('btn-danger')
+      document.querySelector('button').classList.add('btn-primary')
+      document.querySelector('button').innerHTML =
+        'Loading <span class="spinner"><i class="fa fa-spinner fa-spin"></i></span>'
+    }
+  })
 })
