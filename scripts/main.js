77c407947d3ee460c8f9ccc413856317999ca34d
Samuel Weke <wekesamuel@yahoo.com>
2019-09-14 17:08:06 +0100

    Fix more bugs and add new fields to the form  (#7)
    * Fix problem with button color on focus
    
    * Change location to text input from text area and make it required
    
    * Make Gender and Preferred social Media required
    
    * Fix typos
    
    * Added form heading "Volunteer form"
    
    * Forced uppercase for:
    - Name fields
    Forced lowercase for:
    - email field
    
    * Added font-family to form heading
    
    * Fix typo
    
    * Fix
    
    - First name and last name fields to sentence case
    
    * Added the following fields
    
    - Tell us about your previous volunteer experience
    - How long do you want to volunteer with us?
    - Where do you see yourself in five(5) years?
    - How do you think volunteering with us can help you achieve that?
    - List three of your interests or area of expertise
    
    * Fix issues with location autocomplete displaying
    
    * Enabled uppercase for first letter of input fields
    
    * Fix the case issue with firstName, lastName and email

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,37 @@
 'use strict'
+google.maps.event.addDomListener(window, 'load', initialize)
+function initialize () {
+  var input = document.getElementById('location')
+  var autocomplete = new google.maps.places.Autocomplete(input)
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const date = new Date()
   const year = date.getFullYear()
   const yearContent = document.querySelector('#year')
   yearContent.innerHTML = year
+
+  // Capitalize first letter of First Name and Last Name
+  document.querySelector('#firstName').onchange = e => {
+    let val = document.querySelector('#firstName').value
+    RegExp = /\b[a-z]/g
+
+    val = val.charAt(0).toUpperCase() + val.substr(1)
+  }
+
+  document.querySelector('#lastName').onchange = e => {
+    let val = document.querySelector('#lastName').value
+    RegExp = /\b[a-z]/g
+
+    val = val.charAt(0).toUpperCase() + val.substr(1)
+  }
+
+  document.querySelector('#email').onchange = e => {
+    let val = document.querySelector('#email').value
+    RegExp = /\b[a-z]/g
+
+    val = val.toLowerCase()
+  }
 
   // Add intl-tel-input
   window.intlTelInputGlobals.loadUtils('scripts/utils.js')
@@ -35,7 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
       e.preventDefault()
 
       // change the button color and add the loading class
-    //   document.querySelector('button').classList.remove('btn-danger')
+      //   document.querySelector('button').classList.remove('btn-danger')
       document.querySelector('button').classList.add('btn-primary')
       document.querySelector('button').innerHTML =
         'Loading <span class="spinner"><i class="fa fa-spinner fa-spin"></i></span>'
